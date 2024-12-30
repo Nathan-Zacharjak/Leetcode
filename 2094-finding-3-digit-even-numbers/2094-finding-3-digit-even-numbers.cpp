11f7cdccfa98ea3576ff456944a5b88d375a3ff5
Nathan Zacharjak <nzac066@outlook.com>
2024-12-30 17:25:10 +1030

    Time: 3 ms (79.97%), Space: 12.4 MB (92.16%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -3,51 +3,6 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(const vector<int>& digits) {
-        // std::map<int, int> digitBank{};
-
-        // for (int i = 0; i < 10; i++){
-        //     digitBank.insert({i, 0});
-        // }
-
-        // for (const auto &digit : digits){
-        //     digitBank.at(digit) += 1;
-        // }
-
-
-        // std::vector<int> result{};
-
-        // for (int i = 1; i < 10; i++){
-        //     if (digitBank.at(i) <= 0){
-        //         continue;
-        //     }
-        //     digitBank.at(i)--;
-
-        //     int firstNum = i * 100;
-
-        //     for (int j = 0; j < 10; j++){
-        //         if (digitBank.at(j) <= 0){
-        //             continue;
-        //         }
-        //         digitBank.at(j)--;
-
-        //         int sum = firstNum + j * 10;
-
-        //         for (int k = 0; k < 10; k+=2){
-        //             if (digitBank.at(k) <= 0){
-        //                 continue;
-        //             }
-
-        //             result.push_back(sum + k);
-        //         }
-
-        //         digitBank.at(j)++;
-        //     }
-
-        //     digitBank.at(i)++;
-        // }
-
-        // return result;
-
         // Create a digit bank holding the frequencies of each digit from the given array
         std::array<int,10> digitBank{0};
         for (const auto &digit : digits) digitBank.at(digit)++;
