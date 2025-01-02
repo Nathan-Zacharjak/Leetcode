cbfff420cb27328c6e57b02985926006fb5b215c
Nathan Zacharjak <nzac066@outlook.com>
2025-01-02 10:38:52 +1030

    Time: 757 ms (5.47%), Space: 212.5 MB (7.81%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -0,0 +1,45 @@
+#include <print>
+
+class NumberContainers {
+private:
+    unordered_map<int, int> container;
+    unordered_map<int, set<int>> indexes;
+
+public:
+    NumberContainers() {
+
+    }
+    
+    void change(int index, int number) {
+        println("Change: Index: {}, Number: {}", index, number);
+
+        if (container.contains(index)){
+            int oldNum = container.at(index);
+            indexes.at(oldNum).erase(index);
+
+            if (indexes.at(oldNum).empty()){
+                indexes.erase(oldNum);
+            }
+        }
+
+        container[index] = number;
+        indexes[number].insert(index);
+    }
+    
+    int find(int number) {
+        println("Find: Number: {}", number);
+
+        if (indexes.contains(number)){
+            return *indexes.at(number).begin();
+        }
+
+        return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
