57194f400dfc05d3a633c78adacb802308a99fa4
Nathan Zacharjak <nzac066@outlook.com>
2025-01-10 14:57:14 +1030

    Time: 1724 ms (6.68%), Space: 420.9 MB (19.41%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,11 +1,9 @@
-typedef long long ll;
-
 class Solution {
 public:
     int checkRecord(int n) {
         const int MOD = 1000000007;
-        vector<vector<ll>> currDPState = vector<vector<ll>>(2, vector<ll>(3, 0));
-        vector<vector<ll>> nextDPState = vector<vector<ll>>(2, vector<ll>(3, 0));
+        vector<vector<int>> currDPState = vector<vector<int>>(2, vector<int>(3, 0));
+        vector<vector<int>> nextDPState = vector<vector<int>>(2, vector<int>(3, 0));
         currDPState[0][0] = 1;
 
         for (int len = 0; len < n; len++){
@@ -13,29 +11,29 @@ public:
                 for (int consecutiveLates = 0; consecutiveLates <= 2; consecutiveLates++){
                     // Choosing A
                     if (absentCount < 1){
-                        nextDPState[absentCount + 1][0] += currDPState[absentCount][consecutiveLates] % MOD;
+                        nextDPState[absentCount + 1][0] = (nextDPState[absentCount + 1][0] + currDPState[absentCount][consecutiveLates]) % MOD;
                     }
                     // Choosing L
                     if (consecutiveLates < 2){
-                        nextDPState[absentCount][consecutiveLates + 1] += currDPState[absentCount][consecutiveLates] % MOD;
+                        nextDPState[absentCount][consecutiveLates + 1] = (nextDPState[absentCount][consecutiveLates + 1] + currDPState[absentCount][consecutiveLates]) % MOD;
                     }
                     // Choosing P
-                    nextDPState[absentCount][0] += currDPState[absentCount][consecutiveLates] % MOD;
+                    nextDPState[absentCount][0] = (nextDPState[absentCount][0] + currDPState[absentCount][consecutiveLates]) % MOD;
                 }
             }
 
             currDPState = nextDPState;
             // Reset the next DP state to 0!
-            nextDPState = vector<vector<ll>>(2, vector<ll>(3, 0));
+            nextDPState = vector<vector<int>>(2, vector<int>(3, 0));
         }
 
-        long long result = 0;
+        int result = 0;
         for (int i = 0; i < 2; i++){
             for (int j = 0; j < 3; j++){
-                result += currDPState[i][j] % MOD;
+                result = (result + currDPState[i][j]) % MOD;
             }
         }
 
-        return result % MOD;
+        return result;
     }
 };
