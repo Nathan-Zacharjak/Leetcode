265c3e5c55a04d51f87566b1e3736af02cdc3a81
Nathan Zacharjak <nzac066@outlook.com>
2025-01-15 16:33:57 +1030

    Time: 4 ms (11.68%), Space: 15.7 MB (5.61%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king) {
+        unordered_set<string> queenSet;
+        for (const auto &queen : queens){
+            queenSet.insert(to_string(queen[0]) + to_string(queen[1]));
+        }
+
+        vector<vector<int>> directions = {{0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}};
+        // N, NE, E, SE, S, SW, W, NW
+        array<bool, 8> blockedDirections = {false};
+        vector<vector<int>> unblockedQueens;
+
+        for (int i = 1; i < 8; i++){
+            for (int dirIndex = 0; dirIndex < 8; dirIndex++){
+                if (blockedDirections.at(dirIndex)){
+                    continue;
+                }
+                int x = directions[dirIndex][0] * i + king[0];
+                int y = directions[dirIndex][1] * i + king[1];
+                string coords = to_string(x) + to_string(y);
+
+                if (queenSet.contains(coords)){
+                    blockedDirections.at(dirIndex) = true;
+                    unblockedQueens.push_back({x, y});
+                }
+            }
+        }
+
+        return unblockedQueens;
+    }
+};
