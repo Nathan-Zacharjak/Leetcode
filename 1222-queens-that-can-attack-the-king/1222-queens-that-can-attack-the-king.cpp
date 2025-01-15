8b084de6569405c42ccf37a260994174e593b409
Nathan Zacharjak <nzac066@outlook.com>
2025-01-15 16:37:05 +1030

    Time: 1 ms (27.1%), Space: 15.9 MB (5.61%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.cpp
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.cpp
@@ -9,11 +9,15 @@ public:
         vector<vector<int>> directions = {{0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}};
         // N, NE, E, SE, S, SW, W, NW
         array<bool, 8> blockedDirections = {false};
+        int blockedCount = 0;
         vector<vector<int>> unblockedQueens;
 
         for (int i = 1; i < 8; i++){
             for (int dirIndex = 0; dirIndex < 8; dirIndex++){
                 if (blockedDirections.at(dirIndex)){
+                    if (blockedCount == 8){
+                        return unblockedQueens;
+                    }
                     continue;
                 }
                 int x = directions[dirIndex][0] * i + king[0];
@@ -22,6 +26,7 @@ public:
 
                 if (queenSet.contains(coords)){
                     blockedDirections.at(dirIndex) = true;
+                    blockedCount++;
                     unblockedQueens.push_back({x, y});
                 }
             }
