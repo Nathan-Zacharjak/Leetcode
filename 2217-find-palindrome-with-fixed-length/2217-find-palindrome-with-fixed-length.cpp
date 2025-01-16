43caa4895a1057142e179333cfbd0dfe9b55159f
Nathan Zacharjak <nzac066@outlook.com>
2025-01-16 15:58:30 +1030

    Time: 43 ms (58.95%), Space: 60.2 MB (29.47%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -2,29 +2,24 @@ class Solution {
 public:
     vector<long long> kthPalindrome(vector<int>& queries, int intLength) {
         vector<long long> answer;
-        bool isOdd = intLength % 2 != 0;
-        int mirrorSize = intLength / 2;
-        if (isOdd){
-            mirrorSize += 1;
-        }
-
-        string maxQueryString(mirrorSize, '9');
-        long long maxQuery = stoll(maxQueryString);
+        int mirrorSize = (intLength + 1) / 2;
+        bool isOdd = intLength % 2 == 1;
+        long long min = pow(10, mirrorSize - 1);
+        long long max = pow(10, mirrorSize) - 1;
 
         for (const auto &query : queries){
-            long long mirrorNum = pow(10, mirrorSize - 1) + query - 1;
-            string mirrorString = to_string(mirrorNum);
-
-            if (mirrorNum > maxQuery){
+            if (query > 9 * min){
                 answer.push_back(-1);
                 continue;
             }
 
+            string mirrorString = to_string(min + query - 1);
             string mirrorString2 = mirrorString;
+
             if (isOdd){
                 mirrorString2.pop_back();
             }
-
+            
             reverse(mirrorString2.begin(), mirrorString2.end());
             mirrorString += mirrorString2;
 
