d9d1f74a8208f5db0978f2801521feb6e931cd3c
Nathan Zacharjak <nzac066@outlook.com>
2025-01-20 17:29:52 +1030

    Time: 0 ms (100%), Space: 12.1 MB (82.84%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,64 +1,33 @@
-#include <print>
-
 class Solution {
-private:
-    bool hasDupe(const string& str1, const string& str2){
-        vector<bool> charSet(26, false);
-
-        for (const auto& c: str1){
-            if (charSet[c - 'a']){
-                return true;
-            }
-            charSet[c - 'a'] = true;
-        }
-
-        for (const auto & c: str2){
-            if (charSet[c - 'a']){
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    unordered_map<string, int> memo;
-    // int callCount = 0;
-
-    int maxForStart(const int& next, string& stringSoFar, const int& n, const vector<string>& arr){
-        if (next == n){
-            return stringSoFar.size();
-        }
-
-        string memoID = to_string(next) + stringSoFar;
-
-        if (memo.contains(memoID)){
-            return memo[memoID];
-        }
-        // println("next: {}, stringSoFar: {}", next, stringSoFar);
-        // callCount++;
-
-        int maxSoFar = 0;
-
-        for (int i = next; i < n; i++){
-            string nextString = stringSoFar;
-            if (!hasDupe(arr[i], stringSoFar)){
-                nextString += arr[i];
-            }
-
-            int result = maxForStart(i + 1, nextString, n, arr);
-            maxSoFar = result > maxSoFar ? result : maxSoFar;
-        }
-
-        memo[memoID] = maxSoFar;
-
-        return maxSoFar;
-    }
-
 public:
     int maxLength(vector<string>& arr) {
-        string stringSoFar;
-        // int result = maxForStart(0, stringSoFar, arr.size(), arr);
-        // println("Count: {}", callCount);
-        return maxForStart(0, stringSoFar, arr.size(), arr);
+        vector<bitset<26>> results = {bitset<26>()};
+        int maxChars = 0;
+
+        for (const auto& str: arr){
+            bitset<26> charBits;
+            bool dupeStr = false;
+            for (const auto& c: str){
+                if (charBits[c - 'a'] == 1){
+                    dupeStr = true;
+                    break;
+                }
+                charBits.set(c - 'a');
+            }
+            if (dupeStr){
+                continue;
+            }
+
+            for (int i = results.size() - 1; i >= 0; i--){
+                if ((charBits & results[i]).any()){
+                    continue;
+                }
+                bitset<26> nextCharBits = charBits | results[i];
+                results.push_back(nextCharBits);
+                maxChars = max(maxChars, (int)nextCharBits.count());
+            }
+        }
+
+        return maxChars;
     }
 };
