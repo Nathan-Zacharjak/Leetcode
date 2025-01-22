29885a69a7b948b984a1b04f2a3e107fd5efc8fb
Nathan Zacharjak <nzac066@outlook.com>
2025-01-22 12:22:24 +1030

    Time: 4 ms (56.44%), Space: 17.2 MB (21.96%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        bool containsNonZero = false;
         vector<string> numsStr;
-        for (const auto& num: nums){
-            numsStr.push_back(to_string(num));
-            if (num != 0){
-                containsNonZero = true;
-            }
-        }
+        for (const auto& num: nums) numsStr.push_back(to_string(num));
 
-        if (!containsNonZero){
-            return "0";
-        }
-
-        sort(numsStr.begin(), numsStr.end(), [](string& str1, string& str2){
-            string concat1 = str1 + str2;
-            string concat2 = str2 + str1;
-            for (int i = 0; i < concat1.size(); i++){
-                if (concat1[i] != concat2[i]){
-                    return concat1[i] > concat2[i];
-                }
-            }
-            return true;
-        });
+        sort(numsStr.begin(), numsStr.end(), [](string& a, string& b){ return a + b > b + a;});
+        if (numsStr[0] == "0") return "0";
 
         string result;
-        for (const auto& numStr: numsStr){
-            result += numStr;
-        }
+        for (const auto& numStr: numsStr) result += numStr;
+
         return result;
     }
 };
