8086bf4bc947832344a131be8b1dd2720ef210da
Nathan Zacharjak <nzac066@outlook.com>
2025-01-24 10:10:42 +1030

    Time: 7 ms (54.04%), Space: 13.8 MB (6.38%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int result = 0;
-        int i = 0;
-        int size = s.size();
+        unordered_map<char, int> value = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int last = s.size() - 1;
+        int total = value[last];
 
-        unordered_map<string, int> costs = {{"I", 1}, {"V", 5}, {"X", 10}, {"L", 50}, {"C", 100}, {"D", 500}, {"M", 1000},
-                                            {"IV", 4}, {"IX", 9}, {"XL", 40}, {"XC", 90}, {"CD", 400}, {"CM", 900}};
-
-        while (i < size){
-            if (i < size - 1 && costs.contains(s.substr(i, 2))){
-                result += costs[s.substr(i, 2)];
-                i++;
+        for (int i = last; i >= 0; i--){
+            if (value[s[i]] < value[s[i + 1]]){
+                total -= value[s[i]];
             } else {
-                result += costs[s.substr(i, 1)];
+                total += value[s[i]];
             }
-            i++;
         }
 
-        return result;
+        return total;
     }
 };
