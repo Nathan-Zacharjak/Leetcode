6705934098eee35ea055a6264f1aa389f6bcbc88
Nathan Zacharjak <nzac066@outlook.com>
2025-01-24 09:58:41 +1030

    Time: 36 ms (5.55%), Space: 18.3 MB (5.29%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int romanToInt(string s) {
         int result = 0;
-        unordered_map<char, int> costs = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-        char lastLetter = '\0';
-        unordered_map<char, pair<char, char>> subCases = {{'I', {'V', 'X'}}, {'X', {'L', 'C'}}, {'C', {'D', 'M'}}};
+        int i = 0;
+        int size = s.size();
 
-        for (const auto& c: s){
-            result += costs[c];
+        unordered_map<string, int> costs = {{"I", 1}, {"V", 5}, {"X", 10}, {"L", 50}, {"C", 100}, {"D", 500}, {"M", 1000},
+                                            {"IV", 4}, {"IX", 9}, {"XL", 40}, {"XC", 90}, {"CD", 400}, {"CM", 900}};
 
-            if (subCases.contains(lastLetter) && (c == subCases[lastLetter].first || c == subCases[lastLetter].second)){
-                result -= costs[lastLetter] * 2;
+        while (i < size){
+            if (i < size - 1 && costs.contains(s.substr(i, 2))){
+                result += costs[s.substr(i, 2)];
+                i++;
+            } else {
+                result += costs[s.substr(i, 1)];
             }
-
-            lastLetter = c;
+            i++;
         }
 
         return result;
