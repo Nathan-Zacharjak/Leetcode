dd91e319f4cfff4973bdce95d131d95f17c9b23d
Nathan Zacharjak <nzac066@outlook.com>
2025-01-24 10:10:50 +1030

    Time: 4 ms (69.44%), Space: 13.5 MB (11.92%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -3,9 +3,9 @@ public:
     int romanToInt(string s) {
         unordered_map<char, int> value = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         int last = s.size() - 1;
-        int total = value[last];
+        int total = value[s[last]];
 
-        for (int i = last; i >= 0; i--){
+        for (int i = last - 1; i >= 0; i--){
             if (value[s[i]] < value[s[i + 1]]){
                 total -= value[s[i]];
             } else {
