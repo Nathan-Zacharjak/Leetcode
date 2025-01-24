d037affa99ee13af95ced3b5f498316720905ea8
Nathan Zacharjak <nzac066@outlook.com>
2025-01-24 17:06:31 +1030

    Time: 3 ms (56.11%), Space: 18.3 MB (5.01%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     string frequencySort(string s) {
+        string answer;
         unordered_map<char, int> sCount;
         for (const auto c: s) sCount[c]++;
 
-        string uniqueChars;
+        vector<string> countToChars(s.size() + 1);
 
         for (const auto& [c, count]: sCount){
-            uniqueChars += c;
+            countToChars[count].push_back(c);
         }
 
-        sort(uniqueChars.begin(), uniqueChars.end(), [&sCount](const auto& c1, const auto& c2){
-            return sCount[c1] > sCount[c2];
-        });
-
-        string answer;
-
-        for (const auto& c: uniqueChars){
-            while (sCount[c] > 0){
-                answer += c;
-                sCount[c]--;
+        for (int i = s.size(); i > 0; i--){
+            for (const auto& c: countToChars[i]){
+                for (int j = 0; j < i; j++){
+                    answer += c;
+                }
             }
         }
 
