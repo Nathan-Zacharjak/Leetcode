6ebc89b4413e864004088be0b8474f4e3a8b1c6c
Nathan Zacharjak <nzac066@outlook.com>
2025-01-27 13:01:56 +1030

    Time: 0 ms (100%), Space: 29.8 MB (89.36%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
-        unordered_set<int> nums;
-        unordered_map<int, int> numCount1;
-        unordered_map<int, int> numCount2;
-        unordered_map<int, int> numCount3;
+        vector<int> bitMasks(101, 0);
         int size1 = nums1.size();
         int size2 = nums2.size();
         int size3 = nums3.size();
@@ -12,35 +9,21 @@ public:
 
         for (int i = 0; i < maxSize; i++){
             if (i < size1){
-                numCount1[nums1[i]]++;
-                nums.insert(nums1[i]);
+                bitMasks[nums1[i]] |= 1 << 0;
             }
             if (i < size2){
-                numCount2[nums2[i]]++;
-                nums.insert(nums2[i]);
+                bitMasks[nums2[i]] |= 1 << 1;
             }
             if (i < size3){
-                numCount3[nums3[i]]++;
-                nums.insert(nums3[i]);
+                bitMasks[nums3[i]] |= 1 << 2;
             }
         }
 
+        // 011 = 3, 101 = 5, 110 = 6, 111 = 7
         vector<int> answer;
-
-        for (const auto& num: nums){
-            int count = 0;
-            if (numCount1.contains(num)){
-                count++;
-            }
-            if (numCount2.contains(num)){
-                count++;
-            }
-            if (numCount3.contains(num)){
-                count++;
-            }
-
-            if (count >= 2){
-                answer.push_back(num);
+        for (int i = 0; i < 101; i++){
+            if (bitMasks[i] == 3 || bitMasks[i] >= 5){
+                answer.push_back(i);
             }
         }
 
