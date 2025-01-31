48ebcb8ba1e1e814a87edcf58f30026d01a9376f
Nathan Zacharjak <nzac066@outlook.com>
2025-01-31 11:51:46 +1030

    Time: 0 ms (100%), Space: 17.5 MB (88.24%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
+        int evenCost = cost[0];
+        int oddCost = cost[1];
 
-        vector<int> minCosts(n);
-        minCosts[0] = cost[0];
-        minCosts[1] = cost[1];
-
-        for (int i = 2; i < n; i++){
-            minCosts[i] = min(minCosts[i - 1], minCosts[i - 2]) + cost[i];
+        for (int i = 2; i < cost.size(); i++){
+            if (i % 2 == 0){
+                evenCost = min(evenCost, oddCost) + cost[i];
+            } else {
+                oddCost = min(evenCost, oddCost) + cost[i];
+            }
         }
 
-        return min(minCosts[cost.size() - 1], minCosts[cost.size() - 2]);
+        return min(evenCost, oddCost);
     }
 };
