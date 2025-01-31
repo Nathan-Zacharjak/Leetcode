f63f225b0ee47b9ee6c73363f43dd0bf5131f408
Nathan Zacharjak <nzac066@outlook.com>
2025-01-31 13:50:43 +1030

    Time: 0 ms (100%), Space: 10 MB (9.82%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int minCostToMoveChips(vector<int>& position) {
         int oddCount = 0;
-        int evenCount = 0;
         for (const auto& pos: position){
             if (pos % 2){
                 oddCount++;
-            } else {
-                evenCount++;
             }
         }
-        return min(oddCount, evenCount);
+        return min(oddCount, (int)position.size() - oddCount);
     }
 };
