adf73929259ab6d0f669e1f28e0cce7112b65503
Nathan Zacharjak <nzac066@outlook.com>
2025-01-31 13:32:53 +1030

    Time: 0 ms (100%), Space: 8 MB (3.8%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -1,35 +1,21 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
-typedef long long ll;
-
 class Solution {
 public:
     int firstBadVersion(int n) {
-        ll left = 1;
-        ll right = n;
-        ll middle = (right + left) / 2;
+        int left = 1;
+        int right = n;
 
-        while (right - left > 1){
-            // 1 -> 1
-            // 2 -> 1
-            // 3 -> 2
-            // 4 -> 2
-            // 5 -> 3, right -> 4, left -> 2
-            // Except for last loop, always have a value on the left of middle
-            bool badMiddle = isBadVersion(middle);
+        while (left < right){
+            int middle = left + (right - left) / 2;
 
-            if (badMiddle){
+            if (isBadVersion(middle)){
                 right = middle;
             } else {
-                left = middle;
+                left = middle + 1;
             }
-            middle = (right + left) / 2;
         }
 
-        if (isBadVersion(middle)){
-            return middle;
-        } else {
-            return middle + 1;
-        }
+        return left;
     }
 };
