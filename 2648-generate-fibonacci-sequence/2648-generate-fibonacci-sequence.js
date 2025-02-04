d65d61c6c2cdcc60c64a6d703b80e2371c8149ce
Nathan Zacharjak <nzac066@outlook.com>
2025-02-04 18:16:04 +1030

    Time: 46 ms (83.62%), Space: 49 MB (55.49%) - LeetHub

--- 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -2,16 +2,14 @@
  * @return {Generator<number>}
  */
 var fibGenerator = function*() {
-    yield 0
-    yield 1
     let num1 = 0
     let num2 = 1
-    let value = 0
+
     while(true){
-        value = num1 + num2
+        yield num1
+        const temp = num1
         num1 = num2
-        num2 = value
-        yield value
+        num2 += temp
     }
 };
 
