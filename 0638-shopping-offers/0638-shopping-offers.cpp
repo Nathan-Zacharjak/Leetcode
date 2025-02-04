c67ef1a53517a9f28d3ba6d46412b6bea6116cd8
Nathan Zacharjak <nzac066@outlook.com>
2025-02-04 17:46:09 +1030

    Time: 4 ms (79.16%), Space: 17 MB (55.57%) - LeetHub

--- 0638-shopping-offers/0638-shopping-offers.cpp
+++ 0638-shopping-offers/0638-shopping-offers.cpp
@@ -1,11 +1,17 @@
 class Solution {
+private:
+    unordered_map<string, int> DPArray;
 public:
     int shoppingOffers(const vector<int>& price, const vector<vector<int>>& specials, vector<int>& needs, const int& currentPrice = 0) {
+        string key;
+
         for (const auto& need: needs){
             if (need < 0){
                 return -1;
             }
+            key += to_string(need) + "#";
         }
+        if (DPArray.contains(key)) return DPArray[key];
 
         int minPrice = 0;
         int n = needs.size();
@@ -26,6 +32,7 @@ public:
             for (int i = 0; i < n; i++) needs[i] += special[i];
         }
 
+        DPArray[key] = minPrice;
         return minPrice;
     }
 };
