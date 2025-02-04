a7345c7dee36dfd3842912bbca084ab48cbbbedf
Nathan Zacharjak <nzac066@outlook.com>
2025-02-04 17:35:33 +1030

    Time: 83 ms (21.32%), Space: 14.7 MB (87.54%) - LeetHub

--- 0638-shopping-offers/0638-shopping-offers.cpp
+++ 0638-shopping-offers/0638-shopping-offers.cpp
@@ -1,31 +1,33 @@
 class Solution {
-private:
-    int directPurchace(const vector<int>& price, const vector<int>& needs){
-        int totalPrice = 0;
-        for (int i = 0; i < price.size(); i++){
-            totalPrice += price[i] * needs[i];
-        }
-        return totalPrice;
-    }
 public:
-    int shoppingOffers(const vector<int>& price, const vector<vector<int>>& specials, const vector<int>& needs, const int& lastSpecialIndex = 0) {
-        int minPrice = directPurchace(price, needs);
+    int shoppingOffers(const vector<int>& price, const vector<vector<int>>& specials, vector<int>& needs, const int& currentPrice = 0) {
+        for (const auto& need: needs){
+            if (need < 0){
+                return -1;
+            }
+        }
 
-        vector<int> newNeeds;
-        for (int i = lastSpecialIndex; i < specials.size(); i++){
-            newNeeds.clear();
-            vector<int> special = specials[i];
+        int minPrice = 0;
+        int n = needs.size();
+        for (int i = 0; i < n; i++){
+            minPrice += price[i] * needs[i];
+        }
+
+        for (const auto& special: specials){
+            int specialPrice = special.back();
+            if (currentPrice + specialPrice >= minPrice) continue;
 
             for (int i = 0; i < needs.size(); i++){
-                if (needs[i] < special[i]){
-                    newNeeds.clear();
-                    break;
-                }
-                newNeeds.push_back(needs[i] - special[i]);
+                needs[i] -= special[i];
             }
 
-            if (!newNeeds.empty()){
-                minPrice = min(minPrice, shoppingOffers(price, specials, newNeeds, i) + special[special.size() - 1]);
+            int result = shoppingOffers(price, specials, needs);
+            if (result != -1){
+                minPrice = min(minPrice, result + specialPrice);
+            }
+
+            for (int i = 0; i < needs.size(); i++){
+                needs[i] += special[i];
             }
         }
 
