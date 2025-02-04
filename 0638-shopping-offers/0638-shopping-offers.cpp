f8746623570f77e4135e275382558e656456ec2f
Nathan Zacharjak <nzac066@outlook.com>
2025-02-04 17:17:48 +1030

    Time: 2427 ms (5.03%), Space: 472 MB (5.03%) - LeetHub

--- 0638-shopping-offers/0638-shopping-offers.cpp
+++ 0638-shopping-offers/0638-shopping-offers.cpp
@@ -1,66 +1,34 @@
 class Solution {
 private:
-    unordered_map<string, int> DPArray;
-
-    int DPHelper(const vector<int>& price, const vector<vector<int>>& specials, vector<int>& needs, int& currentPrice){
-        bool allItemsBought = true;
-        for (const auto& need: needs){
-            if (need > 0){
-                allItemsBought = false;
-                break;
-            }
-        }
-        if (allItemsBought) return currentPrice;
-
-        string key;
-        for (const auto& need: needs){
-            key += to_string(need) + "#";
-        }
-
-        key += to_string(currentPrice);
-        if (DPArray.contains(key)) return DPArray[key];
-
-        int minPrice = currentPrice;
-        int n = price.size();
-
-        for (const auto &special: specials){
-            int specialPrice = special[n];
-            int normalPrice = 0;
-            bool invalidPurchase = false;
-            vector<int> newNeeds = needs;
-
-            for (int i = 0; i < n; i++){
-                newNeeds[i] -= special[i];
-                normalPrice += special[i] * price[i];
-
-                if (newNeeds[i] < 0){
-                    invalidPurchase = true;
-                    break;
-                }
-            }
-            if (invalidPurchase) continue;
-
-            int saving = normalPrice - specialPrice;
-            int newPrice = currentPrice - saving;
-            
-            int result = DPHelper(price, specials, newNeeds, newPrice);
-            if (result < minPrice){
-                minPrice = result;
-            }
-        }
-
-        DPArray[key] = minPrice;
-        return minPrice;
-    }
-
-public:
-    int shoppingOffers(vector<int>& price, vector<vector<int>>& specials, vector<int>& needs) {
+    int directPurchace(const vector<int>& price, const vector<int>& needs){
         int totalPrice = 0;
-
         for (int i = 0; i < price.size(); i++){
             totalPrice += price[i] * needs[i];
         }
+        return totalPrice;
+    }
+public:
+    int shoppingOffers(const vector<int>& price, const vector<vector<int>>& specials, const vector<int>& needs, const int& lastSpecialIndex = 0) {
+        int minPrice = directPurchace(price, needs);
 
-        return DPHelper(price, specials, needs, totalPrice);
+        vector<int> newNeeds;
+        for (int i = lastSpecialIndex; i < specials.size(); i++){
+            newNeeds.clear();
+            vector<int> special = specials[i];
+
+            for (int i = 0; i < needs.size(); i++){
+                if (needs[i] < special[i]){
+                    newNeeds.clear();
+                    break;
+                }
+                newNeeds.push_back(needs[i] - special[i]);
+            }
+
+            if (!newNeeds.empty()){
+                minPrice = min(minPrice, shoppingOffers(price, specials, newNeeds, i) + special[special.size() - 1]);
+            }
+        }
+
+        return minPrice;
     }
 };
