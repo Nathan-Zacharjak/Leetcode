3c89de5367d34ce663fae4d589cf682fd548cf2b
Nathan Zacharjak <nzac066@outlook.com>
2025-02-05 12:31:05 +1030

    Time: 0 ms (100%), Space: 7.8 MB (44.62%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int bottlesDrunk = 0;
-        int emptyBottles = 0;
-        int fullBottles = numBottles;
-
-        do {
-            bottlesDrunk += fullBottles;
-            emptyBottles += fullBottles;
-
-            fullBottles = emptyBottles / numExchange;
-            emptyBottles = emptyBottles % numExchange;
-        } while (fullBottles > 0);
-
-        return bottlesDrunk;
+        return numBottles + (numBottles - 1)/(numExchange - 1);
     }
 };
