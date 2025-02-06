3681898cfd9e8a1e4e28e70262f14f79af14fb1e
Nathan Zacharjak <nzac066@outlook.com>
2025-02-06 16:18:34 +1030

    Time: 22 ms (25.86%), Space: 28.6 MB (42.6%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+    vector<string> sortPeople(vector<string>& names, const vector<int>& heights) {
+        int n = names.size();
         priority_queue<pair<int, string>> nameQueue;
-        for (auto i = 0; i < heights.size(); i++){
+
+        for (auto i = 0; i < n; i++){
             nameQueue.push({heights[i], names[i]});
         }
 
-        vector<string> sortedNames;
-        while (!nameQueue.empty()){
-            sortedNames.push_back(nameQueue.top().second);
+        for (auto i = 0; i < n; i++){
+            names[i] = nameQueue.top().second;
             nameQueue.pop();
         }
 
-        return sortedNames;
+        return names;
     }
 };
