b34d0d778817e2e3ee1975164322df0c78df62d6
Nathan Zacharjak <nzac066@outlook.com>
2025-02-06 14:32:29 +1030

    Time: 0 ms (100%), Space: 11.5 MB (12.83%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,45 @@
+// #include <print>
+
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        // Try to make each number as large as possible
+        // Each row has as many 1s to the left as possible
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> oneCounts(n, 0);
+
+        for (auto& row: grid){
+            bool firstZero = row[0] == 0;
+
+            for (auto i = 0; i < n; i++){
+                if (firstZero){
+                    row[i] = row[i] == 0 ? 1 : 0;
+                }
+                if (row[i] == 1) oneCounts[i]++;
+            }
+        }
+
+        // for (auto i = 0; i < n; i++){
+        //     println("index: {}, oneCount: {}", i, oneCounts[i]);
+        //     for (const auto& col: grid[i]){
+        //         print("{} ", col);
+        //     }
+        //     print("\n");
+        // }
+
+        // Try to make each column have as many 1s as possible
+        int score = 0;
+        int powerOf2 = 1 << n - 1;
+        // println("powerOf2 : {}", powerOf2);
+
+        for (const auto& count: oneCounts){
+            int bestCount = max(m - count, count);
+            score += powerOf2 * bestCount;
+            powerOf2 = powerOf2 >> 1;
+        }
+
+        return score;
+    }
+};
