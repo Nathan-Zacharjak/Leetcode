99a3bfda2271d7c26fe47ebc2db4daab484791e0
Nathan Zacharjak <nzac066@outlook.com>
2025-02-07 18:15:03 +1030

    Time: 0 ms (100%), Space: 7.8 MB (94.43%) - LeetHub

--- 1175-prime-arrangements/1175-prime-arrangements.cpp
+++ 1175-prime-arrangements/1175-prime-arrangements.cpp
@@ -2,20 +2,31 @@ class Solution {
 public:
     int numPrimeArrangements(int n) {
         const long long MOD = 1e9 + 7;
-        vector<bool> primes(n + 1, true);
+        // vector<bool> primes(n + 1, true);
 
-        for (int p = 2; p * p <= n; p++){
-            if (!primes[p]) continue;
-            
-            // Mark all multiples of the prime, up to n,
-            // numbers before the square of the prime are already marked!
-            for (int i = p * p; i <= n; i += p) primes[i] = false;
-        }
+        // for (int p = 2; p * p <= n; p++){
+        //     if (!primes[p]) continue;
+
+        //     // Mark all multiples of the prime, up to n,
+        //     // numbers before the square of the prime are already marked!
+        //     for (int i = p * p; i <= n; i += p) primes[i] = false;
+        // }
 
         int primeCount = 0;
+        // for (int i = 2; i <= n; i++){
+        //     if (primes[i]) primeCount++;
+        // }
         for (int i = 2; i <= n; i++){
-            if (primes[i]) primeCount++;
+            bool isPrime = true;
+            for (int j = 2; j * j <= i; j++){
+                if (i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) primeCount++;
         }
+
 
         int nonPrimeCount = n - primeCount;
         int maxCount = max(primeCount, nonPrimeCount);
