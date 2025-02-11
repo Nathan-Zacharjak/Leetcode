616b8101b1167540ea68625e263038a312d3d31c
Nathan Zacharjak <nzac066@outlook.com>
2025-02-11 17:22:48 +1030

    Time: 12 ms (77.27%), Space: 18.5 MB (36.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,86 @@
+#include <print>
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> lis;
+        vector<int> fakeLIS;
+        vector<int> fakeLISIndexes;
+        vector<int> ancestors(nums.size(), -1);
+        
+        for (auto i = 0; i < nums.size(); i++){
+            int num = nums[i];
 
-        for (const auto& num: nums){
+            if (fakeLIS.empty() || fakeLIS.back() < num){
+                if (!fakeLIS.empty()){
+                    // Because the current number is building a new longest subsequence
+                    // We can just link back to the last index we inserted, that will be the previous number in the sequence
+                    ancestors[i] = fakeLISIndexes.back();
+                }
 
-            if (lis.empty() || lis.back() < num){
-                lis.push_back(num);
+                fakeLIS.push_back(num);
+                fakeLISIndexes.push_back(i);
                 continue;
             }
-            if (lis.back() == num || lis.front() == num) continue;
+            if (fakeLIS.back() == num || fakeLIS.front() == num) continue;
 
-            int left = 0, right = lis.size() - 1, middle = -1;
+            int left = 0, right = fakeLIS.size() - 1, middle = -1;
             while (left < right){
                 middle = left + (right - left)/2;
 
-                if (lis[middle] < num){
+                if (fakeLIS[middle] < num){
                     left = middle + 1;
                 } else {
                     right = middle;
                 }
             }
+
+            // So, if we're wanting to create a new subsequence branch,
+            // we need to make the current number's ancestor the last number smaller than it
+            // this is given by the number before the one the binary search returns
+
+            // Also, if the current number is smaller than all numbers seen in the sequence so far, we just have a brand new sequence,
+            // so since the ancestors array got intilised to -1s, we can just skip trying to find the first valid number to make the new sequence
+            if (left > 0){
+                // since the binary search gives the first invalid number's index, the first valid number will be 1 before this
+                ancestors[i] = fakeLISIndexes[left - 1];
+            }
             
-            lis[left] = num;
+            fakeLIS[left] = num;
+            fakeLISIndexes[left] = i;
         }
 
-        return lis.size();
+        // println("Fake LIS:");
+        // for (const auto& n: fakeLIS){
+        //     print("{} ", n);
+        // }
+        // print("\n");
+
+        // println("Fake LIS Indexes:");
+        // for (const auto& n: fakeLISIndexes){
+        //     print("{} ", n);
+        // }
+        // print("\n");
+
+        // println("Ancestors:");
+        // for (auto i = 0; i < ancestors.size(); i++){
+        //     println("numsIndex: {} ancestor: {}", i, ancestors[i]);
+        // }
+        // print("\n");
+
+        int currentIndex = fakeLISIndexes.back();
+        vector<int> lis;
+        while (currentIndex != -1){
+            lis.push_back(nums[currentIndex]);
+            currentIndex = ancestors[currentIndex];
+        }
+        reverse(lis.begin(), lis.end());
+
+        println("LIS:");
+        for (const auto& n: lis){
+            print("{} ", n);
+        }
+        print("\n");
+
+        return fakeLIS.size();
     }
 };
