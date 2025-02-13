336adee16acd51b9543513d8d1722d5763584142
Nathan Zacharjak <nzac066@outlook.com>
2025-02-13 11:44:12 +1030

    Time: 12 ms (28.47%), Space: 97.3 MB (22.38%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,7 +1,16 @@
+// #include <print>
+
 class Solution {
-public:
-    bool validPartition(const vector<int>& nums, const int& start = 0) {
+private:
+    bool DPHelper(const vector<int>& nums, const int& start, vector<int>& DPArray) {
+        // println("Start: {}, DPArray:", start);
+        // for (auto i = 0; i < DPArray.size(); i++){
+        //     println("i: {}, value: {}", i, DPArray[i]);
+        // }
+
         if (start == nums.size()) return true;
+
+        if (DPArray[start] != -1) return DPArray[start];
 
         // First check the first 2 elements if they are the same
         bool is2Equal = false;
@@ -9,15 +18,23 @@ public:
         bool is3Increasing = false;
 
         if (nums.size() > start + 1 && nums[start] == nums[start + 1]){
-            is2Equal = validPartition(nums, start + 2);
+            is2Equal = DPHelper(nums, start + 2, DPArray);
         }
         if (nums.size() > start + 2 && nums[start] == nums[start + 1] && nums[start] == nums[start + 2]){
-            is3Equal = validPartition(nums, start + 3);
+            is3Equal = DPHelper(nums, start + 3, DPArray);
         }
         if (nums.size() > start + 2 && nums[start] == nums[start + 1] - 1 && nums[start + 1] == nums[start + 2] - 1){
-            is3Increasing = validPartition(nums, start + 3);
+            is3Increasing = DPHelper(nums, start + 3, DPArray);
         }
 
-        return is2Equal || is3Equal || is3Increasing;
+        return DPArray[start] = is2Equal || is3Equal || is3Increasing;
+    }
+
+public:
+    bool validPartition(const vector<int>& nums, const int& start = 0) {
+        vector<int> DPArray(nums.size() + 3, -1);
+
+        return DPHelper(nums, 0, DPArray);
+        // return false;
     }
 };
