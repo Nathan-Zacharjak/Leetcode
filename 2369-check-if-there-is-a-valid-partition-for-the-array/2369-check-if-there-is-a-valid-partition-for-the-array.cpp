f6cef8f555312b367b9a076c4cf49c8eac49ce56
Nathan Zacharjak <nzac066@outlook.com>
2025-02-13 13:06:25 +1030

    Time: 0 ms (100%), Space: 87.3 MB (99.03%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -70,6 +70,7 @@ public:
             threeSkipPair = twoSkipPair;
             twoSkipPair = oneSkipPair;
             oneSkipPair = viable;
+            
             if (oneSkipPair.first || oneSkipPair.second || twoSkipPair.first || twoSkipPair.second || threeSkipPair.first || threeSkipPair.second){
                 continue;
             } else {
