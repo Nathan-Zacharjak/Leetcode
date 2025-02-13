87d7d6d9007027296fe85b74eac6fca9a4e7cd4a
Nathan Zacharjak <nzac066@outlook.com>
2025-02-13 11:44:25 +1030

    Time: 7 ms (61.31%), Space: 97.5 MB (17.03%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,18 +1,10 @@
-// #include <print>
-
 class Solution {
 private:
     bool DPHelper(const vector<int>& nums, const int& start, vector<int>& DPArray) {
-        // println("Start: {}, DPArray:", start);
-        // for (auto i = 0; i < DPArray.size(); i++){
-        //     println("i: {}, value: {}", i, DPArray[i]);
-        // }
-
         if (start == nums.size()) return true;
 
         if (DPArray[start] != -1) return DPArray[start];
 
-        // First check the first 2 elements if they are the same
         bool is2Equal = false;
         bool is3Equal = false;
         bool is3Increasing = false;
@@ -33,8 +25,7 @@ private:
 public:
     bool validPartition(const vector<int>& nums, const int& start = 0) {
         vector<int> DPArray(nums.size() + 3, -1);
-
+        
         return DPHelper(nums, 0, DPArray);
-        // return false;
     }
 };
