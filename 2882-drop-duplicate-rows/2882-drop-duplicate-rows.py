7e779dc57626b7adc4d9c9f9eca97d3a16162aff
Nathan Zacharjak <nzac066@outlook.com>
2025-02-13 15:35:31 +1030

    Time: 591 ms (23.99%), Space: 66.5 MB (23.48%) - LeetHub

--- 2882-drop-duplicate-rows/2882-drop-duplicate-rows.py
+++ 2882-drop-duplicate-rows/2882-drop-duplicate-rows.py
@@ -1,20 +1,4 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    customers.drop_duplicates("email", inplace = True)
-    return customers
-
-    # seenEmails = set()
-    # deleteIds = set()
-    # for id, email in enumerate(customers.email):
-    #     id = id + 1
-    #     print(id, email)
-    #     if email in seenEmails:
-    #         deleteIds.add(id)
-    #     seenEmails.add(email)
-    
-    # for id in deleteIds:
-    #     customers.drop(customers[id == customers.customer_id].index, inplace = True)
-
-    # customers = customers.drop(customers[customers.customer_id in deleteIds].index)
-    # return customers
+    return customers.drop_duplicates("email")
