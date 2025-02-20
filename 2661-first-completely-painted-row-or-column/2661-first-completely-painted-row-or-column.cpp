eaf81ffe890bdab51e9277313446c9d4f9ecfb87
Nathan Zacharjak <nzac066@outlook.com>
2025-02-20 17:32:29 +1030

    Time: 14 ms (79.43%), Space: 134.4 MB (81.29%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -3,33 +3,29 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
+        int size = n * m;
 
-        unordered_map<int, int> numToIndex;
-
-        for (auto i = 0; i < arr.size(); i++){
-            numToIndex[arr[i]] = i;
-        }
-
-        int result = arr.size();
+        vector<int> valToRow(size + 1);
+        vector<int> valToCol(size + 1);
 
         for (auto row = 0; row < n; row++){
-            int maxPaintIndex = -1;
             for (auto col = 0; col < m; col++){
-                int index = numToIndex[mat[row][col]];
-                maxPaintIndex = max(maxPaintIndex, index);
+                int val = mat[row][col];
+                valToRow[val] = row;
+                valToCol[val] = col;
             }
-            result = min(result, maxPaintIndex);
+        }
+        vector<int> rowCount(n);
+        vector<int> colCount(m);
+
+        for (auto i = 0; i < size; i++){
+            int val = arr[i];
+            rowCount[valToRow[val]]++;
+            colCount[valToCol[val]]++;
+
+            if (rowCount[valToRow[val]] == m || colCount[valToCol[val]] == n) return i;
         }
 
-        for (auto col = 0; col < m; col++){
-            int maxPaintIndex = -1;
-            for (auto row = 0; row < n; row++){
-                int index = numToIndex[mat[row][col]];
-                maxPaintIndex = max(maxPaintIndex, index);
-            }
-            result = min(result, maxPaintIndex);
-        }
-
-        return result;
+        return -1;
     }
 };
