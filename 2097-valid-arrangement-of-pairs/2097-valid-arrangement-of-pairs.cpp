e0ffdd363f2670d102ebfec535004c73be7da94d
Nathan Zacharjak <nzac066@outlook.com>
2025-02-21 18:44:10 +1030

    Time: 292 ms (92.36%), Space: 290.6 MB (81.27%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
-        unordered_map<int, deque<int>> adjList;
-        unordered_map<int, int> inDegree, outDegree;
+        unordered_map<int, vector<int>> adjList;
+        unordered_map<int, int> inOutDegree;
 
         for (const auto& pair: pairs){
             adjList[pair[0]].push_back(pair[1]);
-            outDegree[pair[0]]++;
-            inDegree[pair[1]]++;
+            inOutDegree[pair[0]]++;
+            inOutDegree[pair[1]]--;
         }
 
         int startNode = pairs[0][0];
-
-        for (const auto& [node, edges]: adjList){
-            if (outDegree[node] == inDegree[node] + 1){
+        for (const auto& [node, degree]: inOutDegree){
+            if (degree == 1){
                 startNode = node;
                 break;
             }
         }
 
-        vector<int> DFSResult;
+        vector<int> path;
         stack<int> nodeStack;
         nodeStack.push(startNode);
 
         while (!nodeStack.empty()){
-            int node = nodeStack.top();
+            auto& edges = adjList[nodeStack.top()];
 
-            if (!adjList[node].empty()){
-                int nextNode = adjList[node].front();
-                adjList[node].pop_front();
-                nodeStack.push(nextNode);
-            } else {
-                DFSResult.push_back(node);
+            if (edges.empty()){
+                path.push_back(nodeStack.top());;
                 nodeStack.pop();
+            } else {
+                nodeStack.push(edges.back());
+                edges.pop_back();
             }
         }
 
+        int maxPathIndex = path.size() - 1;
         vector<vector<int>> answer;
-        for (auto i = DFSResult.size() - 1; i > 0; i--){
-            answer.push_back({DFSResult[i], DFSResult[i - 1]});
+        answer.reserve(maxPathIndex);
+
+        for (auto i = maxPathIndex; i > 0; i--){
+            answer.push_back({path[i], path[i - 1]});
         }
 
         return answer;
