27e726903faa6f883c5ee36a5569c1d7a03fcc86
Nathan Zacharjak <nzac066@outlook.com>
2025-02-21 18:04:33 +1030

    Time: 551 ms (43.8%), Space: 451.8 MB (23.77%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
@@ -1,49 +1,71 @@
 class Solution {
-private:
-    void DFS(vector<int>& DFSResult, unordered_map<int, unordered_set<int>>& adjListOut, const int& currentNode){
-        while (!adjListOut[currentNode].empty()){
-            int nextNode = *(adjListOut[currentNode].begin());
-            adjListOut[currentNode].erase(nextNode);
+// private:
+//     void DFS(vector<int>& DFSResult, unordered_map<int, unordered_set<int>>& adjListOut, const int& currentNode){
+//         while (!adjListOut[currentNode].empty()){
+//             int nextNode = *(adjListOut[currentNode].begin());
+//             adjListOut[currentNode].erase(nextNode);
 
-            DFS(DFSResult, adjListOut, nextNode);
-        }
+//             DFS(DFSResult, adjListOut, nextNode);
+//         }
 
-        DFSResult.push_back(currentNode);
-    }
+//         DFSResult.push_back(currentNode);
+//     }
 
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
-        vector<vector<int>> answer;
-        unordered_map<int, unordered_set<int>> adjListOut;
-        unordered_map<int, unordered_set<int>> adjListIn;
+        // unordered_map<int, unordered_set<int>> adjListOut;
+        // unordered_map<int, unordered_set<int>> adjListIn;
+        unordered_map<int, deque<int>> adjList;
+        unordered_map<int, int> inDegree, outDegree;
 
-        for (auto i = 0; i < pairs.size(); i++){
-            adjListOut[pairs[i][0]].insert(pairs[i][1]);
-            adjListIn[pairs[i][1]].insert(pairs[i][0]);
+        for (const auto& pair: pairs){
+            // adjListOut[pairs[i][0]].insert(pairs[i][1]);
+            // adjListIn[pairs[i][1]].insert(pairs[i][0]);
+            adjList[pair[0]].push_back(pair[1]);
+            outDegree[pair[0]]++;
+            inDegree[pair[1]]++;
         }
 
-        bool outDegreeMatchesIn = true;
-        int nodeWithOneMoreOutToIn = -1;
+        // bool outDegreeMatchesIn = true;
+        int startNode = -1;
 
-        for (const auto& [node, edges]: adjListOut){
-            if (adjListOut[node].size() == adjListIn[node].size() + 1){
-                outDegreeMatchesIn = false;
-                nodeWithOneMoreOutToIn = node;
+        for (const auto& [node, edges]: adjList){
+            // if (adjListOut[node].size() == adjListIn[node].size() + 1){
+            if (outDegree[node] == inDegree[node] + 1){
+                // outDegreeMatchesIn = false;
+                startNode = node;
                 break;
             }
         }
 
-        if (outDegreeMatchesIn) nodeWithOneMoreOutToIn = pairs[0][0];
+        if (startNode == -1) startNode = pairs[0][0];
 
         vector<int> DFSResult;
-        DFS(DFSResult, adjListOut, nodeWithOneMoreOutToIn);
+        stack<int> nodeStack;
+        nodeStack.push(startNode);
+
+        while (!nodeStack.empty()){
+            int node = nodeStack.top();
+
+            if (!adjList[node].empty()){
+                int nextNode = adjList[node].front();
+                adjList[node].pop_front();
+                nodeStack.push(nextNode);
+            } else {
+                DFSResult.push_back(nodeStack.top());
+                nodeStack.pop();
+            }
+        }
+
+        // DFS(DFSResult, adjListOut, nodeWithOneMoreOutToIn);
         reverse(DFSResult.begin(), DFSResult.end());
 
-        int lastNode = DFSResult[0];
+        // int lastNode = DFSResult[0];
+        vector<vector<int>> answer;
         for (auto i = 1; i < DFSResult.size(); i++){
-            int node = DFSResult[i];
-            answer.push_back({lastNode, node});
-            lastNode = node;
+            // int node = DFSResult[i];
+            answer.push_back({DFSResult[i - 1], DFSResult[i]});
+            // lastNode = node;
         }
 
         return answer;
